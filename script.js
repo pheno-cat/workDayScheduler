76ac75ff422fc440a97725e815ca0fd1649724db
pheno-cat <catherinejalomo@gmail.com>
2020-10-27 15:09:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var timeblockEl = $(".container");
+var currentExactTime = moment().format("L");
+
+$("#currentDay").text(currentExactTime);
+
+var startDay = 9
+var endDay = 17
+var currentTime = moment().format("H");
+console.log(currentTime)
+
+for(i = startDay; i<=endDay; i++){
+    var newRow = $("<div>")
+    newRow.attr("class", "row")
+
+    var newP = $("<p>");
+    newP.attr("class", "col-1")
+    newP.text(i);
+    newRow.append(newP)
+
+    var newTextArea = $("<textarea>");
+    if(i < currentTime){
+        newTextArea.attr("class", "col-10 past")
+    } else if(i== currentTime){
+        newTextArea.attr("class", "col-10 present")
+    } else {
+        newTextArea.attr("class", "col-10 future")
+    }
+    var info = localStorage.getItem(i)
+    newTextArea.text(info)
+    newRow.append(newTextArea);
+
+    var newBtn = $("<button>");
+    newBtn.text("Save")
+    newBtn.attr("class", "col-1 saveBtn")
+    newBtn.attr("id", i)
+    newRow.append(newBtn)
+    
+    timeblockEl.append(newRow)
+}
+
+function saveInfo() {
+    var newKey = $(this).attr("id")
+    var newVal = $(this).prev().val()
+    localStorage.setItem(newKey, newVal)
+}
+
+$(document).on("click", "button", saveInfo)
